b422807f229d7f84a0d8eaf532a6b1a15b8f86b8
Thomas de Graaff <t.de.graaff@vu.nl>
2021-06-24 14:03:14 +0200

    this looks better

--- code/make_table_example.R
+++ code/make_table_example.R
@@ -1,15 +1,19 @@
 library(stargazer)
 
 ## 2 OLS models
-linear.1 <- lm(rating ~ complaints + privileges + learning + raises + critical,
+linear.1 <- lm(rating ~ complaints + privileges + learning + 
+                   raises + critical,
                data=attitude)
-linear.2 <- lm(rating ~ complaints + privileges + learning, data=attitude)
+linear.2 <- lm(rating ~ complaints + privileges + learning, 
+               data=attitude)
 ## create an indicator dependent variable, and run a probit model
 attitude$high.rating <- (attitude$rating > 70)
-probit.model <- glm(high.rating ~ learning + critical + advance, data=attitude,
+probit.model <- glm(high.rating ~ learning + critical + 
+                        advance, data=attitude,
                     family = binomial(link = "probit"))
 
-stargazer(linear.1, linear.2, probit.model, type =  "text", title="Results", align=TRUE)
+stargazer(linear.1, linear.2, probit.model, 
+          type =  "text", title="Results", align=TRUE)
 
 
 
